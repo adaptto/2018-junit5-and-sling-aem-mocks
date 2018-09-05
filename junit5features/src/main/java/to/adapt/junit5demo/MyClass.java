0c62f12f82f3286db59972d6c4b82a1d44b14689
sseifert <sseifert@pro-vision.de>
2018-09-05 16:48:24 +0200

    separate example projects

---
+++ junit5features/src/main/java/to/adapt/junit5demo/MyClass.java
@@ -0,0 +1,15 @@
+package to.adapt.junit5demo;
+
+/**
+ * My class
+ */
+public class MyClass {
+
+  /**
+   * My method
+   */
+  public void doIt() {
+    // do nothing
+  }
+
+}
---
+++ junit5features/src/test/java/to/adapt/junit5demo/MyClassTest.java
@@ -0,0 +1,18 @@
+package to.adapt.junit5demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MyClassTest {
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    // TODO: implement
+  }
+
+  @Test
+  public void testDoIt() throws Exception {
+    // TODO: implement
+  }
+
+}
