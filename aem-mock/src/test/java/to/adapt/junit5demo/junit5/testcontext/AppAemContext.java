3baf66504a55c712ed43244b3a05d42ccb794dbb
sseifert <sseifert@pro-vision.de>
2018-09-07 13:48:57 +0200

    add caconfig plugins as example

--- aem-mock/src/test/java/to/adapt/junit5demo/junit5/testcontext/AppAemContext.java
+++ aem-mock/src/test/java/to/adapt/junit5demo/junit5/testcontext/AppAemContext.java
@@ -1,5 +1,8 @@
 package to.adapt.junit5demo.junit5.testcontext;
 
+import static io.wcm.testing.mock.wcmio.caconfig.ContextPlugins.WCMIO_CACONFIG;
+import static org.apache.sling.testing.mock.caconfig.ContextPlugins.CACONFIG;
+
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextBuilder;
 
@@ -14,6 +17,10 @@ public final class AppAemContext {
 
   public static AemContext newAemContext() {
     return new AemContextBuilder()
+        // register plugins
+        .plugin(CACONFIG)
+        .plugin(WCMIO_CACONFIG)
+        // shared context setup code for all tests
         .<AemContext>afterSetUp(context -> {
           // register sling models
           context.addModelsForPackage("to.adapt.junit5demo");
